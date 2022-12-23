aba68f0213fb187ac7190319ba0f7ac124edb09d
Tadej Glinšek <tadej.glinsek@gmail.com>
2022-12-23 12:18:52 +0100

    Nadgradil 2D tabelo

--- pridobivanje_podatkov/knjižnice/tabela.py
+++ pridobivanje_podatkov/knjižnice/tabela.py
@@ -3,6 +3,7 @@ class Vrsta():
     def __init__(self, tabela, indeks):
         self.tabela = tabela
         self.indeks = indeks
+        self.jaz = lambda:self.tabela.get_row_copied(self.indeks)  # za metode, ki jih bomo podedovali iz list razreda
     
     def __repr__(self):
         return str(self.tabela.get_row_copied(self.indeks))
@@ -12,6 +13,9 @@ class Vrsta():
         self.tabela.safer_set(default, self.tabela.width() - 1 + n, self.indeks, default)
     
     def append(self, el):
+        if el is None:
+            el = self.tabela.get_default()
+        
         self.tabela.safer_set(el, self.tabela.width(), self.indeks)
 
     def __len__(self):
@@ -30,19 +34,36 @@ class Vrsta():
         return self.tabela.get(ključ, self.indeks)
 
     def __setitem__(self, ključ, vrednost):
+        if type(ključ) == slice:
+            raise NotImplementedError
+        
         # self.tabela.array[self.indeks][ključ] = vrednost
         self.tabela.set(vrednost, ključ, self.indeks)
 
     def __delitem__(self, ključ):
+        if type(ključ) == slice:
+            raise NotImplementedError
+
         # self.tabela.array[self.indeks][ključ] = None
         self.tabela.del_el(ključ, self.indeks)
-
+    
+    def extend(self, sez):
+        for i in sez:
+            self.append(i)
+    
+    def index(self, el):
+        return self.jaz().index(el)
+    
+    def reverse(self):
+        self.tabela.array[self.indeks] = self[::-1]
+    
 
 class Stolpec():
 
     def __init__(self, tabela, indeks):
         self.tabela = tabela
         self.indeks = indeks
+        self.jaz = lambda:self.tabela.get_column_copied(self.indeks)
     
     def __repr__(self):
         return str(self.tabela.get_column_copied(self.indeks)) + "^T"
@@ -52,6 +73,9 @@ class Stolpec():
         self.tabela.safer_set(default, self.indeks, self.tabela.height() - 1 + n, default)
     
     def append(self, el):
+        if el is None:
+            el = self.tabela.get_default()
+        
         self.tabela.safer_set(el, self.indeks, self.tabela.height())
     
     def __len__(self):
@@ -67,22 +91,43 @@ class Stolpec():
         return self.tabela.get(self.indeks, ključ)
 
     def __setitem__(self, ključ, vrednost):
+        if type(ključ) == slice:
+            raise NotImplementedError
+
         self.tabela.set(vrednost, self.indeks, ključ)
 
     def __delitem__(self, ključ):
+        if type(ključ) == slice:
+            raise NotImplementedError
+
         self.tabela.del_el(self.indeks, ključ)
+
+    def extend(self, sez):
+        for i in sez:
+            self.append(i)
+
+    def index(self, el):
+        return self.jaz().index(el)
+    
+    def reverse(self):
+        self[:] = self[::-1]
 
 
 class Tabela:
+    # vse funkcije so narete tako, da preprečujejo, da bo None kdajkoli element matrike
+    # bolje je namesto None imeti kak drug default, odvisno pač od tega, kakšni so ostali elementi v matriki
+    # uporabljamo metodo get_default
 
     def __init__(self, vsebina=[], y=None):
+        # y je tudi default element, če vsebina ni pravokotna matrika
         self.array = []
         
-        if y is not None:
+        if y is not None and type(vsebina) == int:
             x = vsebina
             self.add_rows(y)
             self.add_columns(x)
             return
+        default = y
         
         števila = True
         for i in vsebina:
@@ -104,8 +149,9 @@ class Tabela:
                 for j in i:
                     self.array[-1].append(j)
         
-        if not self.check_if_rectangular(): raise Exception("Vrstice niso enako dolge!")
-        
+        #if not self.check_if_rectangular(): raise Exception("Vrstice niso enako dolge!")
+        self.make_rectangular(default)
+
     def __repr__(self):
         # return str(self.array)
         l = "[\n"
@@ -117,6 +163,39 @@ class Tabela:
     def check_if_rectangular(self):
         # preveri, da vse vrstice enako dolge
         return len({len(vrsta) for vrsta in self.array}) <= 1
+
+    def make_rectangular(self, default=None, zahtevana_širina=None):
+        if zahtevana_širina is None:
+            zahtevana_širina = len(max(self.array, key=len))
+        
+        if default is None:
+            default = self.get_default()
+        
+        for vrsta in self.array:
+            vrsta.extend([default] * (zahtevana_širina - len(vrsta)))
+
+    def get_default(self, vzorec=None):
+        # vzorec naj ne bo None: to je le, če ne podamo parametra
+        
+        slovar = {type(None):None, str:"-", int:0, float:0.0, list:[], dict:{}, set:set(), tuple:tuple(), bool:False}
+
+        if vzorec is None:
+            if len(self.array) > 0 and len(self.array[0]) > 0:
+                a = self.array[0][0]
+                """
+                if type(a) in [list, dict, set]:
+                    return a.copy()
+                return a
+                """
+                return slovar[type(a)]
+            return None
+        
+        if type(vzorec) == type:
+            tip = vzorec
+        else:
+            tip = type(vzorec)
+        
+        return slovar[tip]
 
     def get_empty(self, koord=(0, 0), y=None):
         if y is None:
@@ -176,12 +255,20 @@ class Tabela:
         if n < 0:
             return
             # raise Exception(f"Število vrstic mora biti nenegativno število, ne pa {n}!")
+        
+        if default is None:
+            default = self.get_default()
+        
         self.array.extend([[default] * self.width() for _ in range(n)])
     
     def add_columns(self, n=1, default=None):
         if n < 0:
             return
             # raise Exception(f"Število stolpcev mora biti nenegativno število, ne pa {n}!")
+        
+        if default is None:
+            default = self.get_default()
+        
         # self.array = [vrsta + [default] * n for vrsta in self.array]
         for vrsta in self.array:
             vrsta.extend([default] * n)
@@ -193,11 +280,14 @@ class Tabela:
             x = koord
         
         if 0 <= y < len(self.array) and 0 <= x < len(self.array[y]):
-            self.array[y][x] = vrednost
+            self.array[y][x] = self.get_default() if vrednost is None else vrednost
         else:
             raise Exception(f"Koordinate {(x, y)} presegajo dimenzije tabele: {(self.width(), self.height())}!")
     
     def __getitem__(self, indeks):
+        if type(indeks) == slice:
+            raise NotImplementedError
+
         return self.get_row(indeks)
 
     def set_row(self, indeks, vrednost):
@@ -207,11 +297,16 @@ class Tabela:
             raise Exception(f"Dolžina seznama mora biti {self.width()}, je pa {len(vrednost)}!")
         
         if 0 <= indeks < self.height():
+            if None in vrednost:
+                vrednost = list(lambda x: self.get_default() if x is None else x, vrednost)
             self.array[indeks] = vrednost
         else:
             raise Exception(f"Indeks {indeks} presega višino tabele: {self.height()}")
     
     def __setitem__(self, indeks, vrednost):
+        if type(indeks) == slice:
+            raise NotImplementedError
+
         self.set_row(indeks, vrednost)
     
     def set_column(self, indeks, vrednost):
@@ -222,7 +317,7 @@ class Tabela:
         
         if 0 <= indeks < self.width():
             for i, el in enumerate(vrednost):
-                self.array[i][indeks] = el
+                self.array[i][indeks] = self.get_default() if el is None else el
         else:
             raise Exception(f"Indeks {indeks} presega širino tabele: {self.width()}")
 
@@ -244,9 +339,10 @@ class Tabela:
             raise Exception(f"Vsaj ena izmed koordinat je negativna: {(x, y)}")
         
         if 0 <= y < len(self.array) and 0 <= x < len(self.array[y]):
-            self.array[y][x] = vrednost
+            self.array[y][x] = self.get_default() if vrednost is None else vrednost
         else:
-            default = ('-' if type(vrednost) == str else (0 if type(vrednost) == int else None)) if default is None else default
+            # default = ('-' if type(vrednost) == str else (0 if type(vrednost) == int else None)) if default is None else default
+            default = self.get_default(vrednost)
             self.add_rows(y - self.height() + 1, default)
             self.add_columns(x - self.width() + 1, default)
             self.array[y][x] = vrednost
@@ -258,6 +354,9 @@ class Tabela:
             raise Exception(f"Indeks {indeks} izven dometa: {[0, self.height()]}")
         
     def __delitem__(self, indeks):
+        if type(indeks) == slice:
+            raise NotImplementedError
+
         self.del_row(indeks)
     
     def del_column(self, indeks):
@@ -274,7 +373,7 @@ class Tabela:
             x = koord
         
         if 0 <= y < len(self.array) and 0 <= x < len(self.array[y]):
-            self.array[y][x] = None
+            self.array[y][x] = self.get_default()
         else:
             raise Exception(f"Koordinata {(x, y)} je izven dometa! Dimenzije tabele: {(self.width(), self.height())}")
 
@@ -320,17 +419,75 @@ class Tabela:
         return iter(self.array)
 
     def iter_row(self, indeks):
-        return iter(self.get_row_copied(indeks))
+        # return iter(self.get_row_copied(indeks))
+        return enumerate(iter(self.get_row_copied(indeks)))
     
     def iter_row_reverse(self, indeks):  # TODO: kako narediti iterator, ki vrača elemente vrstice, in pa tudi indeks, le da je indeks takšen, kot je v starševski tabeli (torej, če iteriramo po reverse vrstici, bodo indeksi padali)
-        return iter(self.get_row_copied(indeks)[::-1])
-    
+        #return iter(self.get_row_copied(indeks)[::-1])
+        return reversed(list(enumerate(iter(self.get_row_copied(indeks)[::-1]))))
+
     def iter_column(self, indeks):
-        return iter(self.get_column_copied(indeks))
+        return enumerate(iter(self.get_column_copied(indeks)))
     
     def iter_column_reverse(self, indeks):
-        return iter(self.get_column_copied(indeks)[::-1])
+        return reversed(list(enumerate(iter(self.get_column_copied(indeks)[::-1]))))
     
+    def iter_from_a_point(self, koord, smer):
+        def fun():
+            # nonlocal koord
+            # nonlocal smer
+            x, y = koord
+            x_, y_ = smer
+            while True:
+                el = self.get(x, y)
+                if el is None:
+                    yield (x, y), el
+                    return
+                yield (x, y), el
+                x += x_
+                y += y_
+        return fun()
+    
+    def iter_from_a_point_wrap(self, koord, smer):
+        def fun():
+            # nonlocal koord
+            # nonlocal smer
+            x, y = koord
+            x_, y_ = smer
+            while True:
+                el = self.get(x, y)
+                if el is None:  # šli smo ven
+                    # yield (x, y), el
+                    # return
+                    
+                    if x < 0:
+                        x += self.width()
+                    elif x >= self.width():
+                        x -= self.width()
+                    if y < 0:
+                        y += self.height()
+                    elif y >= self.height():
+                        y -= self.height()
+                    
+                    el = self.get(x, y)
+                    if el is None:
+                        raise Exception(f"Prevelik korak! Korak je {(x_, y_)}, koord je {(x, y)}, dimenzije tabele {(self.width(), self.height())}")
+
+                yield (x, y), el
+                x += x_
+                y += y_
+        return fun()
+    
+    def najdi_polje(self, koord, smer, pogoj):
+        for (x, y), el in self.iter_from_a_point(koord, smer):
+            if pogoj(el, (x, y)):
+                return (x, y), el
+    
+    def najdi_polje_wrap(self, koord, smer, pogoj):
+        for (x, y), el in self.iter_from_a_point_wrap(koord, smer):
+            if pogoj(el, (x, y)):
+                return (x, y), el
+
     @staticmethod
     def get_line(par1, par2):
         # vrne seznam koordinat
